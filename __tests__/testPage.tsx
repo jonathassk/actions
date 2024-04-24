23e8b110c30a276ee62e94d8c9bcb63a968849c3
jonathas <jonathas09@gmail.com>
2024-04-23 22:27:04 -0300

    teste funcionando

---
+++ __tests__/testPage.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Page from '../src/app/page'
+import React from 'react';
+
+describe('Home component', () => {
+  test('renders welcome message', () => {
+    const {getByText} = render(<Page/>);
+    const welcomeMessage = getByText('Welcome to your new project!');
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+});
