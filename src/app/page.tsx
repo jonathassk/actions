a0e0a4e146016d13ec83f29d67a861c34ade6298
jonathas <jonathas09@gmail.com>
2024-04-25 21:39:25 -0300

    atualizaçao para verificar se o cache esta funcionando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p>Welcome to your new projects!</p>
+        <p>atualizacao</p>
       </div>
     </main>
   );
